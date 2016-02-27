944074c98b66901fa4a2e8c9fb87acb97879eb37
R.Brown <ready.brown@hotmail.de>
2016-02-27 11:51:48 +0100

    - use SPDX License Identifier for Apache License 2.0

--- netlicensing.php
+++ netlicensing.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @author    Labs64 <netlicensing@labs64.com>
- * @license   Apache License, Version 2.0
+ * @license   Apache-2.0
  * @link      http://netlicensing.io
  * @copyright 2016 Labs64 NetLicensing
  */

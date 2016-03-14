bbc50d79f3bba49d0a5dbbc9de535787cc73980c
Rudkovskiy <xraskolx@gmail.com>
2016-03-14 17:52:13 +0300

    Fix connection License entity and LicenseService

--- netlicensing.php
+++ netlicensing.php
@@ -11,6 +11,7 @@ require_once(__DIR__ . '/RestController/NetLicensingAPI.php');
 
 // Entities
 require_once(__DIR__ . '/Entities/BaseEntity.php');
+require_once(__DIR__ . '/Entities/License.php');
 require_once(__DIR__ . '/Entities/Product.php');
 require_once(__DIR__ . '/Entities/ProductModule.php');
 require_once(__DIR__ . '/Entities/Licensee.php');
@@ -22,5 +23,6 @@ require_once(__DIR__ . '/Services/BaseEntityService.php');
 require_once(__DIR__ . '/Services/ProductService.php');
 require_once(__DIR__ . '/Services/ProductModuleService.php');
 require_once(__DIR__ . '/Services/LicenseeService.php');
+require_once(__DIR__ . '/Services/LicenseService.php');
 require_once(__DIR__ . '/Services/LicenseTemplateService.php');
 require_once(__DIR__ . '/Services/TokenService.php');

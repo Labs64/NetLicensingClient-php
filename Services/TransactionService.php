adb4f33bd7c6d2e987ee00d8acae999e51ad9a84
R.Brown <ready.brown@hotmail.de>
2016-03-16 11:04:02 +0100

    Create TransactionService.php

---
+++ Services/TransactionService.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * @author    Labs64 <netlicensing@labs64.com>
+ * @license   Apache-2.0
+ * @link      http://netlicensing.io
+ * @copyright 2016 Labs64 NetLicensing
+ */
+namespace NetLicensing;
+
+
+class TransactionService extends BaseEntityService
+{
+    const SERVICE_URL = '/transaction';
+
+    public static function connect(NetLicensingAPI $nlic_connect)
+    {
+        return new TransactionService($nlic_connect);
+    }
+
+    public function getList()
+    {
+        return $this->_getList($this->nlic_connect);
+    }
+
+    public function get($number)
+    {
+        return $this->_get($number, $this->nlic_connect);
+    }
+
+    protected function _getNewEntity()
+    {
+        return new Transaction();
+    }
+
+    protected function _getServiceUrlPart()
+    {
+        return self::SERVICE_URL;
+    }
+}

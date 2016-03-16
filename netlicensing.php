f1bace28539ddc3ba18f00f3ec73606994f90161
R.Brown <ready.brown@hotmail.de>
2016-03-16 10:54:42 +0100

    Update netlicensing.php

--- netlicensing.php
+++ netlicensing.php
@@ -11,18 +11,18 @@ require_once(__DIR__ . '/RestController/NetLicensingAPI.php');
 
 // Entities
 require_once(__DIR__ . '/Entities/BaseEntity.php');
-require_once(__DIR__ . '/Entities/License.php');
 require_once(__DIR__ . '/Entities/Product.php');
 require_once(__DIR__ . '/Entities/ProductModule.php');
-require_once(__DIR__ . '/Entities/Licensee.php');
 require_once(__DIR__ . '/Entities/LicenseTemplate.php');
+require_once(__DIR__ . '/Entities/Licensee.php');
+require_once(__DIR__ . '/Entities/License.php');
 require_once(__DIR__ . '/Entities/Token.php');
 
 // Services
 require_once(__DIR__ . '/Services/BaseEntityService.php');
 require_once(__DIR__ . '/Services/ProductService.php');
 require_once(__DIR__ . '/Services/ProductModuleService.php');
+require_once(__DIR__ . '/Services/LicenseTemplateService.php');
 require_once(__DIR__ . '/Services/LicenseeService.php');
 require_once(__DIR__ . '/Services/LicenseService.php');
-require_once(__DIR__ . '/Services/LicenseTemplateService.php');
 require_once(__DIR__ . '/Services/TokenService.php');

4a5581f1f6fce32ec4f18f463c918fc7cf4fa44f
Slava Rudkovskiy <xraskolx@gmail.com>
2019-02-18 16:03:52 +0300

    change Transaction converter

--- converter/ItemToTransactionConverter.php
+++ converter/ItemToTransactionConverter.php
@@ -6,6 +6,15 @@ class ItemToTransactionConverter
 {
     public static function convert($item)
     {
-        return new Transaction(ItemToArrayConverter::convert($item));
+        $array = ItemToArrayConverter::convert($item);
+
+        $licenseTransactionJoins = !empty($array['licenseTransactionJoins']) ? $array['licenseTransactionJoins'] : [];
+
+        unset($array['licenseTransactionJoins']);
+
+        $transaction = new Transaction($array);
+        $transaction->setLicenseTransactionJoins($licenseTransactionJoins);
+
+        return $transaction;
     }
 }

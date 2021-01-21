c2a3e96a08e54d4c1e617dc26bf2e76bdc068fd9
Viacheslav Rudkovskiy <xraskolx@gmail.com>
2021-01-21 17:29:55 +0200

    - add offline validation functionality - add offline validation example

---
+++ examples/OfflineValidation.php
@@ -0,0 +1,11 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+class OfflineValidation extends TestCase
+{
+    public function testOfflineValidation()
+    {
+        $this->assertEquals(true, true);
+    }
+}
